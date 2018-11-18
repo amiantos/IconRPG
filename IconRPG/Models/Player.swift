90f13a32729411371c3d00d7112fbb1bd8f834b6
amiantos <bradroot@me.com>
2018-11-17 16:01:08 -0800

    implement gitignore & add realm pod

---
+++ IconRPG/Models/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  IconRPG
+//
+//  Created by Bradley Root on 11/17/18.
+//  Copyright © 2018 Brad Root. All rights reserved.
+//
+
+import Foundation
+
+class Player: Object {
+    
+}
